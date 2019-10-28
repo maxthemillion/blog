f60a124c293bd4ac3e9dcaf4a89e42b296cfc3ec
maxthemillion <max.rahn@tum.de>
2019-10-28 23:09:02 +0100

    foto-vue

--- src/views/Foto.vue
+++ src/views/Foto.vue
@@ -1,5 +1,17 @@
 <template>
-    <div></div>
+    <div class='wrapper flex-h-v'>
+            <div class='content foto'>
+                <div class='centered' id='headline'>
+                    <h3>Fotografie</h3>
+                </div>
+                <div class='centered' id='text'>
+                    This is an even longer test.
+                </div>
+                <div class='centered' id='links'>
+                    This is a third test
+                </div>
+            </div>
+    </div>
 </template>
 
 <script>
@@ -7,3 +19,44 @@ export default {
     name:'Foto'
 }
 </script>
+
+<style scoped>
+.wrapper{
+    min-height:300px;
+}
+
+.centered{
+    width: 80%;
+    margin: 0 auto;
+}
+
+.content{
+    max-width: 400px;
+}
+
+.foto{
+    background:#00dbc2;
+    color: white;
+    width:100%;
+    height:100%;
+    display:flex;
+    flex-direction: column;
+    justify-content:space-around;
+}
+
+#headline{
+    text-align: center;
+}
+
+h3{
+    margin:0;
+}
+
+.text{
+    width:95%;
+    height:95%;
+    margin: auto auto;
+}
+
+
+</style>
