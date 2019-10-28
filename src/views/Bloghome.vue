297e15686cde8b181bc0c5034312dc14425ea82f
maxthemillion <max.rahn@tum.de>
2019-10-28 22:35:27 +0100

    category filtering

--- src/views/Bloghome.vue
+++ src/views/Bloghome.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
-  <div class="sticky wrapper flex-h-v" v-if="!isSelected()">
-      <div class='tag tag-normal'>tag-filtering active:</div><div class="tag tag-highlight" @click="resetSelection()">{{'#'+this.selected + ' x'}}</div>
+  <div class="sticky wrapper flex-h-v" v-if="!isTagSelected() || !isCatSelected()">
+      <div v-if="!isTagSelected()" class='tag tag-normal'>tag-filtering active:</div>
+      <div v-if="!isTagSelected()" class="tag tag-highlight" @click="resetTagSelection()">{{'#'+this.tagSelected + ' x'}}</div>
+      <div v-if="!isCatSelected()" class='tag tag-normal'>category-filtering active:</div>
+      <div v-if="!isCatSelected()" class="tag tag-highlight" @click="resetCatSelection()">{{this.catSelected + ' x'}}</div>
   </div>  
   <div class="wrapper flex-h">
     <div class="content">
       <div v-for="(section, index) in Object.keys(entries).sort(function ( a, b ) { return b - a; })" :key="index" class="group">
         <h2 class="center">{{section}}</h2>
-        <div class="section" v-for="entry in entries[section]" :key="entry.id">
-          <div class="entry" v-if="display(entry.ts)">
-            <h3>
-              <router-link :to="{name: entry.id}">
-                {{entry.title}}
-              </router-link>
-            </h3>
-            <span class="date">{{entry.date}}</span>
-            <div class="tags">
-              <div class="tag tag-normal" v-for="(tag, index) in entry.ts" :key="index" @click="setSelected(tag)">
-                {{'#'+tag}}
-              </div>
+        <div v-for="entry in entries[section]" :key="entry.id">
+          <div class="entry-wrapper" v-if="display(entry.ts, entry.cat)">
+            <div class="cat" :style="{background: getCatColor(entry.cat)}" @click="setCatSelected(entry.cat)">
             </div>
-            <p>{{entry.description}}</p>
+            <div class="entry">
+              <h3>
+                <router-link :to="{name: entry.id}">
+                  {{entry.title}}
+                </router-link>
+              </h3>
+              <span class="date">{{entry.date}}</span>
+              <div class="tags">
+                <div class="tag tag-normal" v-for="(tag, index) in entry.ts" :key="index" @click="setTagSelected(tag)">
+                  {{'#'+tag}}
+                </div>
+              </div>
+              <p>{{entry.description}}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -35,25 +42,47 @@ export default {
   name: "bloghome",
   data: function() {
     return {
-      selected: ""
+      tagSelected: "",
+      catSelected: "",
+      colorDict: {
+        foto: "#00dbc2",
+        dataViz: "#0000c9",
+      }
     };
   },
   methods: {
-    setSelected(selection) {
-      this.selected = selection;
+    setTagSelected(selection) {
+      this.tagSelected = selection;
     },
-    display(tags) {
-      if (!this.selected || tags.includes(this.selected)) {
+    setCatSelected(selection) {
+      this.catSelected = selection;
+    },
+    display(tags, cat) {
+      if (
+        (!this.tagSelected && !this.catSelected) ||
+        (!this.catSelected && tags.includes(this.tagSelected)) ||
+        (!this.tagSelected && cat === this.catSelected) ||
+        (tags.includes(this.tagSelected) && cat === this.catSelected)
+      ) {
         return true;
       } else {
         return false;
       }
     },
-    isSelected() {
-      return !this.selected;
+    isTagSelected() {
+      return !this.tagSelected;
     },
-    resetSelection() {
-      this.selected = "";
+    isCatSelected() {
+      return !this.catSelected;
+    },
+    resetTagSelection() {
+      this.tagSelected = "";
+    },
+    resetCatSelection() {
+      this.catSelected = "";
+    },
+    getCatColor(cat) {
+      return this.colorDict[cat];
     }
   },
   computed: {
@@ -66,6 +95,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.cat {
+  margin-right: 1em;
+  flex: 0 0 4px;
+  cursor:pointer;
+  border-bottom: 1em solid white;
+}
+
 .date {
   font-size: 0.8em;
   font-weight: 500;
@@ -78,11 +114,16 @@ h3 {
   margin: 1em 0;
 }
 
+.entry-wrapper {
+  display: flex;
+  flex-direction: row;
+}
+
 .sticky {
   position: sticky;
   top: 3em;
-  background:antiquewhite;
-  height:1.5em;
+  background: antiquewhite;
+  height: 1.5em;
 }
 
 .tags {
@@ -91,7 +132,7 @@ h3 {
   justify-content: left;
 }
 
-.tag{
+.tag {
   padding: 0 0.5em;
   height: 1.3em;
   border-radius: 0.4em;
@@ -99,18 +140,18 @@ h3 {
   font-style: italic;
   margin-right: 0.5em;
   cursor: pointer;
-  background:white;
+  background: white;
 }
 
-.tag:hover{
-  opacity:0.5;
+.tag:hover {
+  opacity: 0.5;
 }
 
 .tag-normal {
   border: 1px solid lightgrey;
 }
 
-.tag-highlight{
+.tag-highlight {
   border: 1px solid #0e84ff;
   color: #0e84ff;
 }
